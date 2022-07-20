c91cf46b9cb888567103ecad9c7e0bde0c7e720b
Vikas Yadav2 <vikas.yadav2@olacabs.com>
2022-07-20 18:19:01 +0530

    how to add more components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Office"/> -->
+  <HelloWorld msg="Welcome to Your Office"/>
   <Home />
 </template>
 
 <script>
 import Home from './components/Home.vue';
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
   
 export default {
   name: 'App',
   components: {
+    HelloWorld,
     Home,
 }
 }
 </script>
 
 <style>
-/* #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} */
+}
 </style>
