a20d147114cfec5ac860bf6ae89f917a128d6e30
Vikas Yadav2 <vikas.yadav2@olacabs.com>
2022-07-27 10:14:47 +0530

    more component added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,28 @@
 <template>
+<div>
 <h1>Home Component</h1>
+<button v-on:mouseover="hello('hello world')">Call hello function</button>
+</div>
 </template>
 <script>
 export default {
     name: "homePage",
+    props:{
+        data:String,
+        msg:String
+    },
+    methods:{
+        hello(data)
+        {
+            alert(data);
+        }
+    }
 }
 </script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
+
