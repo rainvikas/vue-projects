e04e8d2aaee86542a7a0eb8b0dea167305aed26b
Vikas Yadav2 <vikas.yadav2@olacabs.com>
2022-07-28 10:50:38 +0530

    basic curd operation

---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+<div>
+    <h1><u>My First Table Project</u></h1>
+    <table border="10pc" align="center">
+        <thead>
+            <th scope="col">:</th>
+            <th scope="col">S.no</th>
+            <th scope="col">Name</th>
+            <th scope="col">Place</th>
+            <th scope="col">Company</th>
+        </thead>
+        <tr v-for="(user, k) in users" :key="k">
+            <td scope="row" class="trashIconContainer">
+                <button type="button" @click="deleteRow(k,user)">Delete</button>
+            </td>
+            <td>
+                <input type="number" v-model="user.Sno" />
+            </td>
+            <td>
+                <input type="text" v-model="user.Name" />
+            </td>
+            <td>
+                <input type="text" v-model="user.Place" />
+            </td>
+            <td>
+                <input type="text" v-model="user.Company" />
+            </td>
+        </tr>
+    </table>
+    <button type='button' class="btn btn-info" @click="addNewRow">
+        <i class="fas fa-plus-circle"></i>
+        Add
+    </button>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            users: [{
+                Sno: '',
+                Name: '',
+                Place: '',
+                Company: '',
+            }]
+        }
+    },
+    methods: {
+        addNewRow() {
+            this.users.push({
+                Sno: '',
+                Name: '',
+                Place: '',
+                Company: '',
+            });
+        },
+        deleteRow(index,user) {
+            var idx = this.users.indexOf(user);
+            console.log(idx,index);
+            if (idx > -1) {
+                this.users.splice(idx, 1);
+            }
+            this.calculateTotal();
+        },
+        saveInvoice() {
+            console.log(JSON.stringify(this.users));
+        },
+    }
+}
+</script>
+<style scoped>
+div {
+    background-color: rgb(0, 179, 255);
+    text-align: center;
+    height: 500px;
+}
+</style>
